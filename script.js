2b64bee78648eb1b5332595156460652fd496aea
Jerome Decinco <57111980+CodeKaito@users.noreply.github.com>
2023-11-13 18:42:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 //TODO 1. Elenca e descrivi i principali datatypes in Javascript. prova a spiegarli come se volessi farli comprendere ad un bambino.
-//?I principali datatypes in Js sono: numbers, string, boolean, undefined, null.
+I principali datatypes in Js sono: numbers, string, boolean, undefined, null, bigInt e symbol.
+  
+Numbers (Numeri): Questi sono come i mattoncini con cui costruire qualcosa. É possibile avere numeri grandi o piccoli, e puoi farli fare tante cose, come sommare o sottrarre.
+
+String (Stringhe): Questi sono come i fili che usi per fare i braccialetti. Puoi mettere insieme lettere per creare parole e frasi, proprio come infili le perline nel filo.
+
+Boolean (Booleani): Questi sono come i cartellini con "sì" o "no". Puoi usarli per dire se qualcosa è vero o falso, proprio come dire se vuoi cioccolato o no.
+
+Undefined (Non definito): Questo è come dire che non hai ancora deciso quale gioco giocare. Non c'è nulla al suo interno al momento.
+
+Null (Vuoto): Questo è come dire che la tua scatola magica è vuota. Non c'è niente dentro, nemmeno aria!
+
+BigInt (Numeri enormi): Questi sono come i numeri giganti che non riesci nemmeno a scrivere su un foglio di carta normale. Sono per quando hai bisogno di giocattoli veramente grandi.
+
+Symbol (Simboli): Questi sono come sigilli magici che puoi mettere sui tuoi giocattoli per renderli unici. Ogni simbolo è diverso dagli altri.
 
 //TODO 2. Scrivi il codice necessario ad effettuare un'addizione dei due numeri 12 e 20 e salvando il risultato in una variabile.
   const a = 12;
